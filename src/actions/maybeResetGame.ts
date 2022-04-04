d79f8d544df5181043428cd5bc7cba4124135f43
Chet Corcos <ccorcos@gmail.com>
2022-04-04 14:43:30 -0700

    deploy

--- src/actions/maybeResetGame.ts
+++
@@ -1,8 +0,0 @@
-import { TupleDatabaseClientApi } from "tuple-database"
-import { GameSchema, resetGame } from "../GameState"
-
-export function maybeResetGame(db: TupleDatabaseClientApi<GameSchema>) {
-	const response = window.confirm(`Are you want to reset the game?`)
-	if (!response) return
-	resetGame(db)
-}
